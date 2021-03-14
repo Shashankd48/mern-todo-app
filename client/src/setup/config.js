6acf6b92d51d6298dcccca230ab202139b0e9011
Shashank Dubey <jfriday464@gmail.com>
2021-03-14 11:39:50 +0530

    API changed

--- client/src/setup/config.js
+++ client/src/setup/config.js
@@ -1,12 +1,12 @@
-export const api = {
-   auth: "todo/api/auth",
-   profile: "todo/api/profile",
-   baseUrl: "todo/api",
-   host: "/",
-};
-
 // export const api = {
-//    auth: process.env.REACT_APP_AUTHAPI,
-//    profile: process.env.REACT_APP_PROFILEAPI,
-//    baseUrl: process.env.REACT_APP_BASEURL,
+//    auth: "todo/api/auth",
+//    profile: "todo/api/profile",
+//    baseUrl: "todo/api",
+//    host: "/",
 // };
+
+export const api = {
+   auth: process.env.REACT_APP_AUTHAPI,
+   profile: process.env.REACT_APP_PROFILEAPI,
+   baseUrl: process.env.REACT_APP_BASEURL,
+};
